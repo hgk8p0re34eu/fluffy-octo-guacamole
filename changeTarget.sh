435164da03d867c36d5f5ada1ddd84c08f1dea0f
hgk8p0re34eu <114274123+hgk8p0re34eu@users.noreply.github.com>
2024-02-28 18:11:17 -0700

    Update changeTarget.sh
    added -h and --help show usage and exit
    
    removed unnecessary newtarget variable formatting
    
    added check for export target line in .zshrc and then creation of said line

--- changeTarget.sh
+++ changeTarget.sh
@@ -1,13 +1,35 @@
 #!/bin/zsh
 
 #a simple script to automate updating of HTB Academy target IPs to a $target variable
-#TODO: consider adding error handling or support for other shells like bash
+#TODO: consider input filtering or support for other shells like bash
 
-echo "Usage -- changeTarget.sh <targetIP>"
+#if no argument was supplied, print usage
+if [ -z $1 ] || [ $1 = -h ] || [ $1 = --help ]
+then
+	echo "Usage -- changeTarget.sh <targetIP>"
+	exit 0
+fi
 
-newtarget=$(echo -n $1 | sed "s/\./\\\./g")
+#assign supplied argument to a variable for use later
+newtarget=$1
 
-sed -i "s/export target=.*/export target="$newtarget"/" ~/.zshrc
+#check if "export target" exists in ~/.zshrc
+grep "export target" ~/.zshrc 1>/dev/null
+exitstatus=$?
+
+#if it wasn't found, append it
+if [ $exitstatus -eq 1 ]
+then
+	echo "export target="$newtarget"" >> ~/.zshrc
+#if it was found, edit it
+elif [ $exitstatus -eq 0 ]
+then
+	sed -i "s/export target=.*/export target="$newtarget"/" ~/.zshrc
+#or if grep errored out
+else
+	echo "Problem...is ~/.zshrc present? Is there a "target" variable?"
+	exit $exitstatus
+fi
 
 echo "--------------tail -n 1 ~/.zshrc--------------"
 tail -n 1 ~/.zshrc
@@ -15,7 +37,7 @@ tail -n 1 ~/.zshrc
 echo "--------------source ~/.zshrc--------------"
 source ~/.zshrc
 
-echo "--------------echo $target--------------"
+echo "--------------echo \$target--------------"
 echo $target
 
 echo "Please start a new terminal session for changes to take effect"
